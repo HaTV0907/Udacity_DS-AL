6671558bbd66418c8f5915a9107150e7f84ffbec
Ha Tran Van <ha.tranvan0907@gmail.com>
2024-07-20 19:57:29 +0700

    update params validation for file resursion

--- Show_Me_the_Data_Structures/P2_File_Recursion/File_Recusion.py
+++ Show_Me_the_Data_Structures/P2_File_Recursion/File_Recusion.py
@@ -1,7 +1,21 @@
 import os
+from pathlib import Path
 
 def find_files(directory, extension):
     files = []
+    # check invalid directory
+    if None == directory or "" == directory:
+        print ("Invalid input path: None")
+        return
+    # check invalid exension
+    if None == extension or "" == extension:
+        print("invalid extension")
+        return
+    # check directory exist
+    directory_path = Path(directory)
+    if not directory_path.exists():
+        print("directory path does not exist")
+        return
     for filename in os.listdir(directory):
         path = os.path.join(directory, filename)
         if os.path.isfile(path) and filename.endswith(extension):
@@ -13,5 +27,20 @@ def find_files(directory, extension):
 # Example usage
 directory = "./testdir"
 extension = ".c"
+result = find_files(directory, extension)
+print(result)
+# null directory
+directory = None
+extension = ".c"
+result = find_files(directory, extension)
+print(result)
+# empty extension
+directory = "./testdir"
+extension = ""
+result = find_files(directory, extension)
+print(result)
+# not exist file extension
+directory = "./testdir"
+extension = ".py"
 result = find_files(directory, extension)
 print(result)
